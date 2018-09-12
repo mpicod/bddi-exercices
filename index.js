5b120ecaf6fff1fa10cb69cde3a66bdcbdbab661
PICOD Martin <mpicod@g-a102b-15.local>
2018-09-12 12:16:10 +0200

    add pug

--- index.js
+++ index.js
@@ -5,4 +5,21 @@ app.get('/', function(req,res) {
     res.send('Hello World')
 })
 
+app.get('/user/:name', function(req,res) {
+    res.send('Hello ' + req.params.name)
+})
+
+app.use(function (req, res, next) {
+    console.log('time', Date.now())
+    next()
+})
+
+app.get('/user/:name', function(req, res){
+    res.render('index', {
+        message: 'hello' + req.params.name
+    })
+})
+
+app.set('view engine', 'pug')
+
 app.listen(3000)
